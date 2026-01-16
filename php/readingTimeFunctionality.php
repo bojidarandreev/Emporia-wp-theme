67564bdbc2d4de5f43f6288d1b7e0752812c9837
Bozhidar Andreev <33715265+bojidarandreev@users.noreply.github.com>
2026-01-16 09:21:36 +0100

    New features were added to make the website look and work better:
    Special scripts were made to move page titles and important text to the top of the page.
    An accordion (expand/collapse) feature was added for FAQ sections.
    The author’s picture (Gravatar) and name now show up with the post date.
    All comments were turned off everywhere on the site.
    The footer was updated to show a custom copyright.
    Tags now show at the top of posts, not at the bottom.
    In short: The site got new scripts for better layout, easier reading, and a more modern look. Comments were disabled, and the code was organized to be easier to manage.

--- php/readingTimeFunctionality.php
+++ php/readingTimeFunctionality.php
@@ -1,4 +1,4 @@
-function gp_reading_time() {
+<!-- function gp_reading_time() {
 $article = get_post_field( 'post_content', $post->ID ); //gets full text from article
 $wordcount = str_word_count( strip_tags( $article ) ); //removes html tags
 $time = ceil($wordcount / 250); //takes rounded of words divided by 250 words per minute
@@ -9,4 +9,4 @@ $label = " minutes";
 }
 $totalString = $time . $label; //adds time with minute/minutes label
 return $totalString;
-}
+} -->
